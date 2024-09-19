0ef4d55a07dbb87d48089408293e08fd3e6e7576
hcc <hcc@gmail.com>
2024-09-19 14:42:34 +0200

    Añadimos que no tenemos red social fav

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba {
 	public static void main (String args[]){
 	System.out.println("Deporte fav: Baloncesto");
 	System.out.println("Lanzarote");
+	System.out.println("No tengo red social fav");
 	}
 }
